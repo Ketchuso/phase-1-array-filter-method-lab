467e8dd3b7d488a774351bf4eb3c6dd308b89d3a
Jay Davis <jdlikesbsbl@gmail.com>
2025-01-30 20:38:50 -0600

    lab completeteteted :)

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(nameArray, string){
+    let match = nameArray.filter(function(name){
+        return name.toLowerCase() === string.toLowerCase();
+    })
+    return match;
+}
+
+function fuzzyMatch(nameArray, string){
+    let match = nameArray.filter(function(name){
+        return name.charAt(0) === string.charAt(0);
+    })
+    return match;
+}
+
+function matchName(driver, string){
+    let match = driver.filter(function(name){
+        return name.name === string;
+    })
+    return match
+}
